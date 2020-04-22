808cc283accf70ff12ea5db731977c3b02a20f7a
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-22 17:28:38 +0000

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,72 @@
 <template>
   <div id="app">
     <HelloWorld/>
+    <main>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav">
+        <img alt="Vue logo" src="./assets/logo.png" width="50px">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/products" class="nav-link">All Products</router-link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link">About</router-link>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <router-link to="/cart" class="nav-link">
+                <i class="fa fa-shopping-cart mr-1">
+                  <span class="ml-1">{{this.cartProducts.length}}</span>
+                </i>
+              </router-link>
+            </li>
+            <li class="nav-item dropdown" v-if="isLogged()">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >{{this.loggedUser.firstName}}</a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <router-link to="/" class="dropdown-item">Profile</router-link>
+                <!-- <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>-->
+                <router-link
+                  to="/"
+                  class="dropdown-item text-danger"
+                  @click.native="loc_logout"
+                >Logout</router-link>
+              </div>
+            </li>
+            <li class="nav-item" v-if="!isLogged()">
+              <router-link to="/login" class="nav-link">Login</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <router-view/>
+    </main>
   </div>
 </template>
 
