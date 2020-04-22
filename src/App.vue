2d94dcd426af27db2c4c8663cbcd5dd19cf23c54
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-22 17:32:26 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -67,6 +67,23 @@
       </nav>
       <router-view/>
     </main>
+    <footer class="container-fluid footer text-left mt-3">
+      <p class="mr-auto">
+        Developed by:
+        <strong>Mohammed Ismail</strong>
+      </p>
+      <div style="float:right">
+        <a href="mailto:ikismail7@gmail.com" style="margin-right:10px">
+          <i class="fa fa-envelope-open" aria-hidden="true" style="font-size:20px"></i>
+        </a>
+        <a href="https://github.com/ikismail" target="_blank" style="margin-right:10px">
+          <i class="fa fa-github" aria-hidden="true" style="font-size:20px"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/ikismail7/" target="_blank" style="margin-right:10px">
+          <i class="fa fa-linkedin" aria-hidden="true" style="font-size:20px"></i>
+        </a>
+      </div>
+    </footer>
   </div>
 </template>
 
