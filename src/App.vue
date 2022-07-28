5c60ff08c9ce666f76be35f2a96d3b2c65d29043
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-07-28 06:55:33 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,12 @@
 <template>
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <main>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav">
-        <img alt="Vue logo" src="./assets/logo.png" width="50px">
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <router-link to="/" class="nav-link">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/products" class="nav-link">All Products</router-link>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <router-link to="/about" class="nav-link">About</router-link>
-            </li>
-          </ul>
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <router-link to="/cart" class="nav-link">
-                <i class="fa fa-shopping-cart mr-1">
-                  <span class="ml-1">{{this.cartProducts.length}}</span>
-                </i>
-              </router-link>
-            </li>
-            <li class="nav-item dropdown" v-if="isLogged()">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >{{this.loggedUser.firstName}}</a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <router-link to="/" class="dropdown-item">Profile</router-link>
-                <!-- <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>-->
-                <router-link
-                  to="/"
-                  class="dropdown-item text-danger"
-                  @click.native="loc_logout"
-                >Logout</router-link>
-              </div>
-            </li>
-            <li class="nav-item" v-if="!isLogged()">
-              <router-link to="/login" class="nav-link">Login</router-link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <router-view/>
-    </main>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-      <card/>
-    <footer class="container-fluid footer text-left mt-3">
-      <p class="mr-auto">
-        Developed by:
-        <strong>Mohammed Ismail</strong>
-      </p>
-      <div style="float:right">
-        <a href="mailto:ikismail7@gmail.com" style="margin-right:10px">
-          <i class="fa fa-envelope-open" aria-hidden="true" style="font-size:20px"></i>
-        </a>
-        <a href="https://github.com/ikismail" target="_blank" style="margin-right:10px">
-          <i class="fa fa-github" aria-hidden="true" style="font-size:20px"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/ikismail7/" target="_blank" style="margin-right:10px">
-          <i class="fa fa-linkedin" aria-hidden="true" style="font-size:20px"></i>
-        </a>
-      </div>
-    </footer>
+  
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations } from "vuex";
-import {
-  isLoggedIn,
-  getLoggedInUser
-} from "./components/shared/service/authService";
-import HelloWorld from './components/HelloWorld.vue'
-import card from './components/cards/card.vue'
+
 
 
 export default {
@@ -107,43 +17,11 @@ export default {
   },
   name: 'App',
   components: {
-    HelloWorld,
-    card
-  },
-  computed: mapState(["cartProducts", "loggedUser"]),
-  methods: {
-    /* Initially loading the cart products from local storage */
-
-    ...mapMutations(["SET_CART_PRODUCTS", "ADD_LOGGED_USER"]),
-
-    getLocalProducts() {
-      const products = JSON.parse(localStorage.getItem("iki-cart"));
-
-      if (products) {
-        this.SET_CART_PRODUCTS(products);
-      }
-    },
-
-    isLogged() {
-      return isLoggedIn();
-    },
-
-    loc_logout() {
-      localStorage.removeItem("_auth");
-      this.$router.push("/");
-      location.reload();
-    }
-  },
-  created() {
-    this.getLocalProducts();
-
-    const loggedUser = getLoggedInUser();
-
-    this.ADD_LOGGED_USER(loggedUser);
-
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.VUE_APP_BASE_URL);
+    
+    
   }
+ 
+  
 };
 
 </script>
