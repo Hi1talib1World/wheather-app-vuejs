eca111175526d0229613db2edd6fff8888e1f6a0
hi1talib1world <hichamoutaleb7@gmail.com>
2025-01-30 21:32:20 +0100

    Update Historical.vue

---
+++ src/components/Historical.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="historical">
+        <h1>Historical Weather Data</h1>
+        <p>Here you can view historical weather data.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Historical'
+}
+</script>
+
+<style scoped>
+.historical {
+    padding: 20px;
+}
+
+.historical h1 {
+    font-size: 24px;
+    margin-bottom: 10px;
+}
+
+.historical p {
+    font-size: 16px;
+}
+</style>
