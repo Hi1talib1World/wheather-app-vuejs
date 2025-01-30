73eb3e68f30068ad10fa352bf8afd66ff1f4cf83
hi1talib1world <hichamoutaleb7@gmail.com>
2025-01-30 21:32:25 +0100

    Update Settings.vue

---
+++ src/components/Settings.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="settings">
+        <h1>Settings</h1>
+        <p>Configure your application settings here.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Settings'
+}
+</script>
+
+<style scoped>
+.settings {
+    padding: 20px;
+}
+
+h1 {
+    font-size: 24px;
+    margin-bottom: 10px;
+}
+
+p {
+    font-size: 16px;
+}
+</style>
