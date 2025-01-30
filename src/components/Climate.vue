15feb241c95ace63fd09d811d01f9c7f2a5e47f7
hi1talib1world <hichamoutaleb7@gmail.com>
2025-01-30 21:32:13 +0100

    Update Climate.vue

---
+++ src/components/Climate.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="climate">
+        <h1>Climate Component</h1>
+        <p>This is the Climate component of the weather app.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Climate'
+}
+</script>
+
+<style scoped>
+.climate {
+    text-align: center;
+    margin-top: 20px;
+}
+</style>
