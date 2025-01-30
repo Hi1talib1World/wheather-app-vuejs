fbc8ffa1aca9b3fe5307a4f2076b17444ecff7cc
hi1talib1world <hichamoutaleb7@gmail.com>
2025-01-30 21:32:06 +0100

    Update Alerts.vue

---
+++ src/components/Alerts.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="alert">
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Alerts',
+    props: {
+        message: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.alert {
+    padding: 10px;
+    background-color: #f44336;
+    color: white;
+    margin-bottom: 15px;
+}
+</style>
